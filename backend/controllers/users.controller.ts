d05bdc136cc181d76927809958262e12d5af21a0
franconicolini1 <franconicolini111@gmail.com>
2023-11-01 10:25:17 -0300

    Endpoints to get messages and message by id

--- backend/controllers/users.controller.ts
+++ backend/controllers/users.controller.ts
@@ -1,12 +1,45 @@
 import { Request, Response } from 'express'
 import { Messages } from '../models/users.model'
 
-export const createMessage = (req: Request, res: Response) => {
+export const createMessage = async (req: Request, res: Response) => {
   const { user, message } = req.body
   if (!user || !message) return res.status(400).json({ ok: false, msg: 'Missing required fields.' })
 
-  const createdMessage = Messages.create({ user, message })
-  if (!createdMessage) return res.status(500).json({ ok: false, msg: 'Error while creating message.' })
+  try {
+    const createdMessage = await Messages.create({ user, message })
+    if (!createdMessage) return res.status(500).json({ ok: false, msg: 'Error while creating message.' })
+    
+    res.status(201).json({ ok: true, msg: 'Message created' })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ ok: false, msg: 'Error while creating message.' })
+  }
+}
 
-  return res.status(201).json({ ok: true, msg: 'Message created' })
+export const getMessage = async (req: Request, res: Response) => {
+  const { id } = req.params
+  if (!id) return res.status(400).json({ ok: false, msg: 'Missing required fields.' })
+
+  try {
+    const data = await Messages.findByPk(id)
+    if (!data) return res.status(404).json({ ok: false, msg: 'Message not found.' })
+    
+    res.status(200).json({ ok: true, msg: 'Message found', data })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ ok: false, msg: 'Error while getting message.' })
+  }
+}
+
+export const getMessages = async (req: Request, res: Response) => {
+  try {
+
+    const data = await Messages.findAll()
+    if (!data) return res.status(404).json({ ok: false, msg: 'Messages not found.' })
+    
+    return res.status(200).json({ ok: true, msg: 'Messages found', data })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ ok: false, msg: 'Error while getting messages.' })
+  }
 }
--- backend/routes/users.routes.ts
+++ backend/routes/users.routes.ts
@@ -3,6 +3,8 @@ import * as usersController from '../controllers/users.controller'
 
 const usersRouter = Router()
 
+usersRouter.get('/messages', usersController.getMessages)
+usersRouter.get('/messages/:id', usersController.getMessage)
 usersRouter.post('/messages', usersController.createMessage)
 
 export default usersRouter
